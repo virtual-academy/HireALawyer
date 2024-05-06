24bc52536f68caeeae3997a2f8c7ea3df9031fe7
Shahid <virtualacademyofficial@gmail.com>
2024-05-06 18:04:54 +0500

    5MAyClass

---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:[true, "Full name is required"],
+        // unique:[true,"this name is already in use"],
+        default:"Unknown User",
+        // minlength:[4,"Full name must be of 4 or more characters."],
+        // select:false,
+    },
+    email:{
+        type:String,
+        required:[true,"Email is Required"]
+    },
+    password:{
+        type:String,
+        required:[true,"Email is Required"],
+        minlength:[8,"Pasword must be of 8 or more characters."],
+        select:false,
+    },
+    city:{
+        type:String,
+        default:'Ksur'
+    }
+
+});
+
+const UserModel = mongoose.Model('user', userSchema);
+module.exports = UserModel;
